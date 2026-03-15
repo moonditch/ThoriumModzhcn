d44183ed2305ecc2a75e8aacbad87f4f0014e2c5
De1tA5 <179575280+De1tA5@users.noreply.github.com>
2026-03-15 22:22:51 +0800

    修复 未开启诸神黄昏 不会覆盖灾厄原汉化问题
    汉化合集包含2个瑟银本地化文件，实际会冲突，之后也可以转移至Mods.ThoriumModzhcn本地化文件下,目前属于笨方法，后续可能会改成阻拦tml本地化文件加载系统

---
+++ Core/Utilities.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+
+namespace ThoriumModzhcn.Core
+{
+    public static class Utilities
+    {
+        public static void FullTooltipOveride(List<TooltipLine> tooltips, string newTooltip)
+        {
+            for (int index = 0; index < tooltips.Count; ++index)
+            {
+                if (tooltips[index].Mod == "Terraria")
+                {
+                    if (tooltips[index].Name == "Tooltip0")
+                    {
+                        TooltipLine tooltip = tooltips[index];
+                        tooltip.Text = $"{newTooltip}";
+                    }
+                    else if (tooltips[index].Name.Contains("Tooltip"))
+                    {
+                        tooltips[index].Hide();
+                    }
+                }
+            }
+        }
+    }
+}
